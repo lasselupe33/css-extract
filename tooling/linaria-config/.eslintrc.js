5149b4b93ba31a1c92d8932057eeafed2aee4959
Lasse Felskov Agersten <lfa@appear.dk>
2023-10-28 15:36:59 +0200

    chore(tooling): set tooling to private

--- tooling/linaria-config/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-require("@tooling/eslint-config/patch");
-
-process.env.GYL__WORKSPACE_ROOT = __dirname;
-
-module.exports = {
-  extends: [require.resolve("@tooling/eslint-config/recommended")],
-};
